32d6913bba94b6e193175ef438e36acadff3eb8a
Pranav Pakhale <PranavP2@citiustech.com>
2019-07-04 13:47:55 +0530

    first sql file

---
+++ SQLQuery1.sql
@@ -0,0 +1,26 @@
+;with new(ct_id,orders)
+as(
+	select CustomerID,count(SalesOrderID)
+	from Sales.SalesOrderHeader
+	group by CustomerID
+), nq(ct_id,orders,rk)
+as(
+select ct_id,orders, dense_rank() over (order by  orders desc) as RowNumberRank 
+	from new
+)
+
+select *
+from nq
+where rk=2;		
+
+		
+with new(ct_id,orders)
+as(
+	select CustomerID,count(SalesOrderID)
+	from Sales.SalesOrderHeader
+	group by CustomerID
+	
+)
+
+select *
+from Sales.SalesOrderHeader
